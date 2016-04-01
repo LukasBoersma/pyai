c190fe7359150a69bb46679f49efa3282678cf81
Jan <jan>
2016-04-01 20:05:53 +0200

    NeuralAI constructor accepts now arguments for number of layers, size of layers, learning rate,... added a function for epoch learning

--- connectfour/neural.py
+++ connectfour/neural.py
@@ -1,7 +1,8 @@
 import lasagne as L
 import theano as T
 import numpy
-
+import gzip
+import random
 
 import cPickle as pickle
 import os
@@ -11,15 +12,32 @@ import connectfour as cf
 
 class NeuralAI:
 
-    def __init__(self):
+    def __init__(self,
+    layer_sizes= [3*cf.SIZE_X*cf.SIZE_Y,84,42,cf.SIZE_X],
+    learning_rate = 0.01,
+    momentum = 0.5,
+    batch_size = 1):
         # create network
         total_field_size = cf.SIZE_X*cf.SIZE_Y
         self.input_var = T.tensor.matrix('input_var')
-        self.l_in = L.layers.InputLayer(shape=(1,3*total_field_size), input_var = self.input_var)
-        self.l_hidden1 = L.layers.DenseLayer(self.l_in, num_units= total_field_size*8 )
-        self.l_hidden2 = L.layers.DenseLayer(self.l_hidden1, num_units= total_field_size )
-        self.l_hidden3 = L.layers.DenseLayer(self.l_hidden2, num_units= cf.SIZE_X*4 )
-        self.l_out = L.layers.DenseLayer(self.l_hidden3, num_units=cf.SIZE_X)
+        if len(layer_sizes) < 2:
+            raise Exception("You need to specify at least 2 layers")
+        self.l_in = L.layers.InputLayer(shape=(batch_size,layer_sizes[0]), input_var = self.input_var)
+
+        self.l_hidden = []
+        #in two-layer case the output layer is connected directly to the input layer
+        self.l_out = L.layers.DenseLayer(self.l_in, num_units=layer_sizes[-1])
+        if len(layer_sizes) > 2:
+            #first hidden layer is special as it is connected to input layer
+            self.l_hidden.append( L.layers.DenseLayer(self.l_in, num_units= layer_sizes[1] ) )
+            # three layer have been constructed so far:
+            # input, first hidden layer, output (output will be changed later)
+            for l in range(2,len(layer_sizes)-1):
+                self.l_hidden.append(L.layers.DenseLayer(self.l_hidden[-1], num_units= layer_sizes[l] ))
+
+            #self.l_hidden2 = L.layers.DenseLayer(self.l_hidden1, num_units= total_field_size )
+            #self.l_hidden3 = L.layers.DenseLayer(self.l_hidden2, num_units= cf.SIZE_X*4 )
+            self.l_out = L.layers.DenseLayer(self.l_hidden[-1], num_units=layer_sizes[-1])
 
         # define a function f that maps an input input_var to an output reaction
 
@@ -35,40 +53,46 @@ class NeuralAI:
 
         self.loss = self.loss.mean()
 
-        self.updates = L.updates.nesterov_momentum(self.loss, self.params, learning_rate=0.01, momentum=0.9)
+        self.updates = L.updates.nesterov_momentum(self.loss, self.params, learning_rate=learning_rate, momentum=momentum)
         self.train_fn = T.function([self.input_var, self.score], self.loss, updates=self.updates)
-     
+
     def evolve(self):
         matrices = L.layers.get_all_param_values(self.l_out)
-        
+
         mean_val = numpy.mean( [numpy.mean(item.flatten()) for item in matrices])
-        
+
         scale_factor = numpy.absolute(mean_val/15)
         print ("Deviation: " + str(scale_factor) )
-        
+
         for m in matrices:
             for entry in m:
                 entry += numpy.random.normal(scale=scale_factor)
         L.layers.set_all_param_values(self.l_out, matrices)
-        
-    def learn(self, training_data):
-        
-        print(len(training_data))
-              
+
+    def learn(self, training_data,silent = False):
+
+        if not silent:
+            print(len(training_data))
+
         loss_function = T.function([self.input_var, self.score], self.loss)
-        
-        
+
+
 
         for (field, score ) in training_data:
             #print("learning")
             #print(len(field))
             #print(field)
             #print(score)
-            
+
             #print( "loss before fit: " + str(loss_function([field],score ))  )
             self.train_fn([field],score)
             #print( "loss after fit: " + str(loss_function([field],score ))  )
 
+    def learn_epoch(self, training_data, epochs):
+        print("learning " + str(epochs) + " epochs")
+        for i in range(epochs):
+            random.shuffle(training_data)
+            self.learn(training_data, silent = True)
 
     # see https://gist.github.com/senbon/70adf5410950c0dc882b
     # and https://github.com/Lasagne/Lasagne/issues/7
@@ -77,8 +101,8 @@ class NeuralAI:
         #print(len(field))
         #print(field)
         score = self.evaluate_prediction([field])
-        
-        return numpy.argmax(score)	
+
+        return numpy.argmax(score)
 
 
     def read_model_data(self, filename):
@@ -96,3 +120,14 @@ class NeuralAI:
         #filename = '%s.%s' % (filename, PARAM_EXTENSION)
         with open(filename, 'w') as f:
             pickle.dump(data, f)
+
+    def pickle(self, filename):
+        f = gzip.open(filename, 'wb')
+        pickle.dump(self, f)
+        f.close()
+
+    @staticmethod
+    def unpickle(filename):
+        f = gzip.open(filename, 'rb')
+        pickle.load(f)
+        f.close()
