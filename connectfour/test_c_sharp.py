30a233eac17164a320970b17b1f783cc69fd49e6
Jan <jan.rosendahl@posteo.de>
2016-04-02 14:20:48 +0200

    moved network evaluation test in extra function

--- connectfour/test_c_sharp.py
+++ connectfour/test_c_sharp.py
@@ -12,22 +12,26 @@ def load_samples(filename):
         f = open(filename,'r')
     return json.load(f)
 
+def test_network_evaluation(network_file,in_out_file):
+    ai = neural.NeuralAI()
 
-ai = neural.NeuralAI()
+    #ai = neural.NeuralAI.load_json('Networks/network_4x3_random.json')
+    ai = neural.NeuralAI.load_json(network_file)
 
-#ai = neural.NeuralAI.load_json('Networks/network_4x3_random.json')
-ai = neural.NeuralAI.load_json('Networks/network_320x120x17x107_random.json.gz')
+    #samples = load_samples('Networks/samples_4x3_random.json')
+    samples = load_samples(in_out_file)
 
-#samples = load_samples('Networks/samples_4x3_random.json')
-samples = load_samples('Networks/samples_broken_320x120x17x107_random.json.gz')
+    networks_differ = 0
+    for i in range(len(samples['Inputs'])):
+        #print(samples['Inputs'][i])
+        lasagne_output = ai.evaluate_prediction( [ samples['Inputs'][i] ] )
 
-networks_differ = 0
-for i in range(len(samples['Inputs'])):
-    #print(samples['Inputs'][i])
-    lasagne_output = ai.evaluate_prediction( [ samples['Inputs'][i] ] )
+        #print(str(lasagne_output) + ',' + str(samples['Outputs'][i]))
 
-    #print(str(lasagne_output) + ',' + str(samples['Outputs'][i]))
+        if not numpy.allclose(lasagne_output, samples['Outputs'][i]):
+            networks_differ+=1
+    print ('Networks produces different outputs: ' + str(networks_differ))
 
-    if not numpy.allclose(lasagne_output, samples['Outputs'][i]):
-        networks_differ+=1
-print ('Networks produces different outputs: ' + str(networks_differ))
+
+
+test_network_evaluation('Networks/network_320x120x17x107_random.json.gz','Networks/samples_320x120x17x107_random.json.gz')
